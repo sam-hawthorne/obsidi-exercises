5f8284c8be3acbfc19d757f0aaa276f3894bc1e7
Sam Hawthorne <shawthorne.hba2022@ivey.ca>
2022-09-30 10:36:37 -0400

    Add spacing in accordance with coding best practices

--- HelloWorld1/src/currencyConversion.java
+++ HelloWorld1/src/currencyConversion.java
@@ -3,17 +3,21 @@ public class currencyConversion {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("What currency would you like to convert to? \n USD $ \n GBP £ \n EUR € \n Yuan ¥ \n BTC ฿");
 		String currency = sc.nextLine();
+
 		System.out.println("What's the dollar value of your CAD you would like to convert?");
 		float value = sc.nextFloat();
+		sc.close();
+
 		float usd = (float) 0.73;
 		float gbp = (float) 0.68;
 		float eur = (float) 0.76;
 		float yuan = (float) 5.29;
 		float btc = (float) 0.000038;
 		float result = 0;
-		sc.close();
+
 			switch (currency) {
 				case "USD":
 					result = value * usd;
