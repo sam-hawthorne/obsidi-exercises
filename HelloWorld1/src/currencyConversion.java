176c1b3ccd209f2d4b9921108d0084002f03727b
Sam Hawthorne <shawthorne.hba2022@ivey.ca>
2022-09-30 10:39:54 -0400

    Add comments for line explanations

--- HelloWorld1/src/currencyConversion.java
+++ HelloWorld1/src/currencyConversion.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 public class currencyConversion {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // open scanner for user input
 
 		System.out.println("What currency would you like to convert to? \n USD $ \n GBP £ \n EUR € \n Yuan ¥ \n BTC ฿");
-		String currency = sc.nextLine();
+		String currency = sc.nextLine(); // stores value for currency to convert to
 
 		System.out.println("What's the dollar value of your CAD you would like to convert?");
-		float value = sc.nextFloat();
-		sc.close();
+		float value = sc.nextFloat(); // stores dollar input for CAD to convert
+		sc.close(); // close scanner to prevent memory leakage
 
-		float usd = (float) 0.73;
+		float usd = (float) 0.73; // conversion rates as of September 28, 2022
 		float gbp = (float) 0.68;
 		float eur = (float) 0.76;
 		float yuan = (float) 5.29;
 		float btc = (float) 0.000038;
-		float result = 0;
+		float result = 0; // temp value so result can calculate conversion
 
 			switch (currency) {
 				case "USD":
