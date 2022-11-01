40aa20e8350ed627f5c235f8cd5f9a0d73c9f342
Sam Hawthorne <shawthorne.hba2022@ivey.ca>
2022-11-01 13:24:26 -0400

    Add new methods to read, update, delete files

--- feedapp/src/main/java/com/bptn/utils/FileUtils.java
+++ feedapp/src/main/java/com/bptn/utils/FileUtils.java
@@ -4,7 +4,8 @@ import java.nio.file.*;
 import java.util.UUID;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-//import java.util.logging.*;
+import java.io.FileWriter;
+import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,13 +34,48 @@ public class FileUtils {
         }
     }
     
+    public static void readFile() throws FileNotFoundException {
+    	
+    	Path p = Paths.get(HOME + "/test.txt");
+    	if (Files.exists(p)) {
+    		Scanner scanner = new Scanner(new File(p.toUri()));
+    		while (scanner.hasNextLine()) {
+    			String data = scanner.nextLine();
+    			System.out.println(data);
+    		}
+    	}
+    }
+    
+    public static void deleteFile() throws IOException {
+    	Path p1 = Paths.get(HOME + "/fake.txt");
+    	System.out.println(Files.exists(p1));
+    	Files.deleteIfExists(p1);
+    }
+    
+    public static void writeFile() throws IOException {
+    	Path p = Paths.get(HOME + "/testWrite.txt");
+    	if (Files.exists(p)) {
+    		FileWriter writer = new FileWriter(new File(p.toUri()));
+    		writer.write("Yo");
+    		writer.close();
+    	}
+    }
+    
+    public static void updateFile() {
+    	
+    }
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO Auto-generated method stub
         Logger logger = LoggerFactory.getLogger(FileUtils.class);
         logger.debug("Executing");
-        isFileExists();
-        isFileExistsWithExceptionHandling();
-        createFileIfNotExist();
+      
+        FileUtils.isFileExists();
+        FileUtils.isFileExistsWithExceptionHandling();
+        FileUtils.createFileIfNotExist();
+        FileUtils.readFile();
+        FileUtils.writeFile();
+        FileUtils.deleteFile();
      
     }
 
