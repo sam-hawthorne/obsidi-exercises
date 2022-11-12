90ed82deaf2fcf882689f583c260b131d5319d7f
Sam Hawthorne <shawthorne.hba2022@ivey.ca>
2022-11-12 18:59:23 -0500

    Add Friday exercises

--- feedapp/src/main/java/com/bptn/repository/FeedHistoryRepository.java
+++ feedapp/src/main/java/com/bptn/repository/FeedHistoryRepository.java
@@ -1,20 +1,23 @@
 package com.bptn.repository;
 
-import java.util.List;
-
+import com.bptn.models.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bptn.models.History;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface FeedHistoryRepository extends JpaRepository<History, String> {
 
-	List<History> findByUserName(String username);
-	
-	History findByPostId(String postID);
-	
-	History findByPostType(String postType);
-	
-	void deleteByPostType(String postType);
+    List<History> findByUsername(String username);
+
+    History findByPostId(String postID);
+
+    List<History> findByPostType(String postType);
+
+    @Transactional
+    void deleteByPostType(String postType);
+
+
 }
