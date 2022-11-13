290dff85e378599b72537fbdc79d40282d1f916b
Sam Hawthorne <shawthorne.hba2022@ivey.ca>
2022-11-12 19:10:39 -0500

    Add @Repository annotation

--- feedapp/src/main/java/com/bptn/repository/FeedPostRepository.java
+++ feedapp/src/main/java/com/bptn/repository/FeedPostRepository.java
@@ -1,8 +1,10 @@
 package com.bptn.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.bptn.models.Post;
 
-
+@Repository
 public interface FeedPostRepository extends FeedRepository {
 	
 	Post save (Post feed);
